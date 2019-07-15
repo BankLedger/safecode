fefbee36361ef4fe755758ff2570fae95d6be0b9
Scott Arnette <scott.a.arnette@gmail.com>
2019-07-15 11:29:56 -0400

    Using new enviroment variable for docker.

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -13,6 +13,7 @@ function determine-hash() {
         echo "$FILE - $HASH"
     done
     export DETERMINED_HASH=$(echo ${HASHES[*]} | sha1sum | awk '{ print $1 }')
+    export HASHED_IMAGE_TAG="${IMAGE_TAG}-${DETERMINED_HASH}"
 }
 
 function build() {
@@ -22,7 +23,7 @@ function build() {
     [[ $IMAGE_TAG == ubuntu-16.04 ]] && CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE='/tmp/pinned_toolchain.cmake' -DCMAKE_CXX_COMPILER_LAUNCHER=ccache" # pinned only
     [[ $IMAGE_TAG == amazonlinux-2 || $IMAGE_TAG == centos-7 ]] && EXPORTS="export PATH=/usr/lib64/ccache:$PATH &&" || EXPORTS="$EXPORTS export PATH=/usr/lib/ccache:$PATH &&" # ccache needs to come first in the list (devtoolset-8 overrides that if we include this in the Dockerfile)
     # DOCKER
-    docker run --rm -v $(pwd):/eos -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache eosio/producer:ci-${IMAGE_TAG}-${DETERMINED_HASH} bash -c " \
+    docker run --rm -v $(pwd):/eos -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache eosio/producer:ci-${HASHED_IMAGE_TAG} bash -c " \
         $PRE_COMMANDS ccache -s && \
         mkdir /eos/build && cd /eos/build && $EXPORTS cmake -DCMAKE_BUILD_TYPE='Release' -DCORE_SYMBOL_NAME='SYS' -DOPENSSL_ROOT_DIR='/usr/include/openssl' -DBUILD_MONGO_DB_PLUGIN=true $CMAKE_EXTRAS /eos && make -j $(getconf _NPROCESSORS_ONLN) 
         ctest -j$(getconf _NPROCESSORS_ONLN) -LE _tests --output-on-failure -T Test"
@@ -32,8 +33,8 @@ function generate_docker_image() {
     # If we cannot pull the image, we build and push it first.
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     cd ./.cicd
-    docker build -t eosio/producer:ci-${IMAGE_TAG}-${DETERMINED_HASH} -f ./${IMAGE_TAG}.dockerfile .
-    docker push eosio/producer:ci-${IMAGE_TAG}-${DETERMINED_HASH}
+    docker build -t eosio/producer:ci-${HASHED_IMAGE_TAG} -f ./${IMAGE_TAG}.dockerfile .
+    docker push eosio/producer:ci-${HASHED_IMAGE_TAG}
 }
 
 determine-hash ".cicd/${IMAGE_TAG}.dockerfile"
@@ -58,8 +59,8 @@ if [[ "$(uname)" == Darwin ]]; then
 else # linux
     echo 'Detected Linux, building in Docker.'
     echo "Looking for $IMAGE_TAG-$DETERMINED_HASH"
-    echo "$ docker pull eosio/producer:ci-${IMAGE_TAG}-${DETERMINED_HASH}"
-    if docker pull eosio/producer:ci-${IMAGE_TAG}-${DETERMINED_HASH}; then
+    echo "$ docker pull eosio/producer:ci-${HASHED_IMAGE_TAG}"
+    if docker pull eosio/producer:ci-${HASHED_IMAGE_TAG}; then
         build
     else
         generate_docker_image
